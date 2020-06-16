1d34559e51448679037891fafbe4a94a5ca78e97
SahilMaharaj <maharajsahil9@gmail.com>
2020-06-16 18:45:47 +0200

    Re-added nprogress site loader with different main js setup to prevent undefined error

--- src/main.js
+++ src/main.js
@@ -1,6 +1,21 @@
 import DefaultLayout from '~/layouts/Default.vue'
+import NProgress from "nprogress";
+import "nprogress/nprogress.css";
 
 export default function (Vue, { router, head, isClient }) {
+  router.beforeEach((to, from, next) => {
+    if (!to.hash && typeof document !== "undefined") {
+        NProgress.start();
+    }
+    next();
+});
+
+router.afterEach((to, from) => {
+  if (!to.hash && typeof document !== "undefined") {
+      NProgress.done();
+  }
+});  
+  
   Vue.component('Layout', DefaultLayout)
   head.link.push ({
     rel: 'stylesheet preload',
