97734866df2edbcdd8a7114e203f39af26c88500
SahilMaharaj <maharajsahil9@gmail.com>
2020-06-16 18:40:08 +0200

    Remove nprogress site loading indicator

--- src/main.js
+++ src/main.js
@@ -1,16 +1,6 @@
 import DefaultLayout from '~/layouts/Default.vue'
-import NProgress from 'nprogress';
-import 'nprogress/nprogress.css';
 
 export default function (Vue, { router, head, isClient }) {
-  NProgress.configure({showSpinner: false});
-  router.beforeEach((to, from, next) => {
-    NProgress.start()
-    next()
-})
-  router.afterEach((to, from) => {
-    NProgress.done()
-})
   Vue.component('Layout', DefaultLayout)
   head.link.push ({
     rel: 'stylesheet preload',
