c80e25547711cdaf1712946a6f20d28a04e39401
SahilMaharaj <maharajsahil9@gmail.com>
2020-08-04 14:42:37 +0200

    Changed org title from input to p tag

--- src/templates/Organisations.vue
+++ src/templates/Organisations.vue
@@ -6,10 +6,14 @@
             
             <div id="org-form">
                <form action="https://www.form-data.com/_functions/submit/ophk4d66zcnl4r9ox6sr" method="post" >
-                  <input type="text" name="organisation" :value="$page.organisations.title">
+                  <p type="text" name="organisation"><strong>{{ $page.organisations.title }}</strong></p>
+
                   <p><input type="text" name="name" required="required" placeholder="Full Name"/></p>
+
                   <p><input type="text" required="required" name="email" placeholder="Email"/></p>
+
                   <p><textarea name="Comments" required="required" id="comments" cols="40" rows="6" placeholder="Comments"></textarea></p>
+
                   <button type="submit" value="Submit">Submit</button>
                </form>
             </div>
