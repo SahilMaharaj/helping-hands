5b3f94a594948d68d2641f053961cb09683034f6
SahilMaharaj <maharajsahil9@gmail.com>
2020-08-04 14:17:17 +0200

    Added graphql page title query in the form

--- src/templates/Organisations.vue
+++ src/templates/Organisations.vue
@@ -3,10 +3,10 @@
         <div>
             <h1>{{ $page.organisations.title }}</h1>
             <VueRemarkContent />
-            {{ $page.organisations.title }}
+            
             <div id="org-form">
                <form action="https://www.form-data.com/_functions/submit/ophk4d66zcnl4r9ox6sr" method="post" >
-
+                  <p><strong>{{ $page.organisations.title }}</strong></p>
                   <p><input type="text" name="name" required="required" placeholder="Full Name"/></p>
                   <p><input type="text" required="required" name="email" placeholder="Email"/></p>
                   <p><textarea name="Comments" required="required" id="comments" cols="40" rows="6" placeholder="Comments"></textarea></p>
