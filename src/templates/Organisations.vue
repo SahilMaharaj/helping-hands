f4b321e061004ec098131f402260202ab7f2f814
SahilMaharaj <maharajsahil9@gmail.com>
2020-08-04 14:46:17 +0200

    changed p tag to label and added a for attribute

--- src/templates/Organisations.vue
+++ src/templates/Organisations.vue
@@ -6,7 +6,7 @@
             
             <div id="org-form">
                <form action="https://www.form-data.com/_functions/submit/ophk4d66zcnl4r9ox6sr" method="post" >
-                  <p type="text" name="organisation"><strong>{{ $page.organisations.title }}</strong></p>
+                  <label for="orgname" name="organisation"><strong>{{ $page.organisations.title }}</strong></label>
 
                   <p><input type="text" name="name" required="required" placeholder="Full Name"/></p>
 
