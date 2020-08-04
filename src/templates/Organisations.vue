c20cd36d488c4d83ccf778485fcd7eb9cdd65d0d
SahilMaharaj <maharajsahil9@gmail.com>
2020-08-04 14:22:58 +0200

    Made page name as a label with a for attribute

--- src/templates/Organisations.vue
+++ src/templates/Organisations.vue
@@ -6,7 +6,7 @@
             
             <div id="org-form">
                <form action="https://www.form-data.com/_functions/submit/ophk4d66zcnl4r9ox6sr" method="post" >
-                  <p><strong>{{ $page.organisations.title }}</strong></p>
+                  <label for="orgname"><strong>{{ $page.organisations.title }}</strong></label>
                   <p><input type="text" name="name" required="required" placeholder="Full Name"/></p>
                   <p><input type="text" required="required" name="email" placeholder="Email"/></p>
                   <p><textarea name="Comments" required="required" id="comments" cols="40" rows="6" placeholder="Comments"></textarea></p>
