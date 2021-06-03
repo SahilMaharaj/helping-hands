602a761e78227c61132a6d0714832fdb074c0e00
unknown <maharajsahil9@gmail.com>
2021-06-03 16:29:09 +0200

    Fixed footer issue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -24,9 +24,7 @@
                     <g-image class="footer-logo" width="200" height="25" src="~/assets/images/helping-hands-logo-white.svg"></g-image>
                 </div>
             </div>
-            <!--<div class="copyright">
-                Built by <a target="_blank" title="Sahil Maharaj LinkedIn Profile" href="https://www.linkedin.com/in/sahil-maharaj/">Sahil Maharaj</a>->
-            </div>
+            
         </div>
     </footer>
 </template>
